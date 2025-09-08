b8f9649f73771614d852d7926b630301f731035f
nir-amir <repandens@protonmail.com>
2025-09-08 14:28:10 +0200

    WIP logging. Currently failing.

--- src/main/java/game/text_adventure/Main.java
+++ src/main/java/game/text_adventure/Main.java
@@ -1,6 +1,10 @@
 package game.text_adventure;
 
+import game.text_adventure.application.TextAdventure;
+
 public class Main {
     public static void main(String[] args) {
+        TextAdventure game = new TextAdventure();
+        game.run();
     }
 }
--- src/main/java/game/text_adventure/application/TextAdventure.java
+++ src/main/java/game/text_adventure/application/TextAdventure.java
@@ -1,4 +1,16 @@
 package game.text_adventure.application;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Scanner;
+
+@Slf4j
 public class TextAdventure {
+
+    public void run() {
+        log.info("Starting game...");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lorem ipsum Titel");
+    }
 }
