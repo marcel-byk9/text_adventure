c06109d42b69e3667fb4c3c3ba925771bb43dfc6
nir-amir <repandens@protonmail.com>
2025-09-15 14:03:42 +0200

    WIP main loop. Modified story notes

--- src/main/java/game/text_adventure/application/TextAdventure.java
+++ src/main/java/game/text_adventure/application/TextAdventure.java
@@ -13,17 +13,35 @@ public class TextAdventure {
      * Main game loop
      */
     public void run() {
-        log.info("Starting game...");
+        log.debug("Starting game...");
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Lorem ipsum Titel");
+
 
         // TODO Main menu (General layout and info)
+        System.out.println("WIP: Projekttitel\n\n");
+        System.out.println("Ein text-basiertes Rollenspielabenteuer inspiriert vom Universum der Animationsserie 'Avatar: Der Herr der Elemente'");
+
+        pressEnterToContinue(scanner);
+        System.out.println("1. Starte ein neues Abenteuer.\n2. Zeige gespeicherte Spielstände.");
+        System.out.println("Bittw wähle eine Option (1/2)");
 
         // TODO Select between new game or saved game
 
         // TODO Select between loading saved game or deleting saved game
 
+    }
+
+    private void pressEnterToContinue(Scanner scanner)
+    {
+        System.out.println("Drücke Enter um fortzufahren...");
+        try
+        {
+            System.in.read();
+            scanner.nextLine();
+        }
+        catch(Exception ex)
+        {log.error("Unexpected exception:", ex);}
     }
 }
